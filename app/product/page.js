a90a682ff4e3f95ef8dc98712cd0fc8e85043375
haoz20 <zhaonianhao2005@gmail.com>
2025-09-17 21:49:23 +0700

    configure products with DataGrid

--- app/product/page.js
+++ app/product/page.js
@@ -2,19 +2,46 @@
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 export default function Home() {
+
+  const columns = [
+    // { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'code', headerName: 'Code', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'price', headerName: 'Price', width: 150 },
+    {
+      field: 'Action', headerName: 'Action', width: 150,
+      renderCell: (params) => {
+        return (
+          <div>
+            <button onClick={() => startEditMode(params.row)}>📝</button>
+            <button onClick={deleteById(params.row.id)}>❌</button>
+          </div>
+        )
+      }
+    },
+  ]
+
   const API_BASE = process.env.NEXT_PUBLIC_API_URL;
   console.debug("API_BASE", API_BASE);
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const [products, setProducts] = useState([]);
+  const [editMode, setEditMode] = useState(false);
   const [category, setCategory] = useState([]);
 
   async function fetchProducts() {
     const data = await fetch(`${API_BASE}/product`);
     // const data = await fetch(`http://localhost:3000/product`);
     const p = await data.json();
-    setProducts(p);
+    const p2 = p.map((product) => {
+      return {
+        ...product,
+        id: product._id
+      }
+    })
+    setProducts(p2);
   }
 
   async function fetchCategory() {
@@ -22,7 +49,25 @@ export default function Home() {
     const c = await data.json();
     setCategory(c);
   }
+  function handleProductFormSubmit(data) {
+    if (editMode) {
+      // Updating a category
+      fetch(`${API_BASE}/product`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then(() => {
+        stopEditMode();
+        fetchProducts();
+      });
+      return;
+    }
 
+    // Creating a new category
+    createProduct(data);
+  }
   const createProduct = (data) => {
     fetch(`${API_BASE}/product`, {
       method: "POST",
@@ -35,11 +80,27 @@ export default function Home() {
 
   const deleteById = (id) => async () => {
     if (!confirm("Are you sure?")) return;
-    
+
     await fetch(`${API_BASE}/product/${id}`, {
       method: "DELETE",
     });
     fetchProducts();
+  };
+
+  function startEditMode(product) {
+    reset(product);
+    setEditMode(true);
+  }
+
+  function stopEditMode() {
+    reset({
+      code: "",
+      name: "",
+      description: "",
+      price: "",
+      category: "",
+    });
+    setEditMode(false);
   }
 
   useEffect(() => {
@@ -48,9 +109,9 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-row gap-4">
-      <div className="flex-1 w-64 ">
-        <form onSubmit={handleSubmit(createProduct)}>
+    <main className="p-4">
+      <div>
+        <form onSubmit={handleSubmit(handleProductFormSubmit)}>
           <div className="grid grid-cols-2 gap-4 m-4 w-1/2">
             <div>Code:</div>
             <div>
@@ -95,35 +156,76 @@ export default function Home() {
                 className="border border-black w-full"
               >
                 {category.map((c) => (
-                  <option key={c._id} value={c._id}>{c.name}</option>
+                  <option key={c._id} value={c._id}>
+                    {c.name}
+                  </option>
                 ))}
               </select>
             </div>
             <div className="col-span-2">
-              <input
+              <div className="col-span-2 text-right">
+                {editMode ? (
+                  <>
+                    <input
+                      type="submit"
+                      className="italic bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                      value="Update"
+                    />{" "}
+                    <button
+                      onClick={() => stopEditMode()}
+                      className=" italic bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+                    >
+                      Cancel
+                    </button>
+                  </>
+                ) : (
+                  <input
+                    type="submit"
+                    value="Add"
+                    className="w-20 italic bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                  />
+                )}
+              </div>
+              {/* <input
                 type="submit"
                 value="Add"
                 className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              />
+              /> */}
             </div>
           </div>
         </form>
       </div>
-      <div className="border m-4 bg-slate-300 flex-1 w-64">
+      {/* <div className="border m-4 bg-slate-300 flex-1 w-64">
         <h1 className="text-2xl">Products ({products.length})</h1>
         <ul className="list-disc ml-8">
-          {
-            products.map((p) => (
-              <li key={p._id}>
-                <button className="border border-black p-1/2" onClick={deleteById(p._id)}>❌</button>{' '}
-                <Link href={`/product/${p._id}`} className="font-bold">
-                  {p.name}
-                </Link>{" "}
-                - {p.description}
-              </li>
-            ))}
+          {products.map((p) => (
+            <li key={p._id}>
+              <button
+                className="border border-black p-1/2"
+                onClick={() => startEditMode(p)}
+              >
+                📝
+              </button>{" "}
+              <button
+                className="border border-black p-1/2"
+                onClick={deleteById(p._id)}
+              >
+                ❌
+              </button>{" "}
+              <Link href={`/product/${p._id}`} className="font-bold">
+                {p.name}
+              </Link>{" "}
+              - {p.description}
+            </li>
+          ))}
         </ul>
-      </div>
-    </div>
+      </div> */}
+      <div className="mx-4">
+              <DataGrid
+                rows={products}
+                columns={columns}
+              />
+            </div>
+    </main>
   );
 }
--- app/v2/category/page.js
+++ app/v2/category/page.js
@@ -27,10 +27,10 @@ export default function Home() {
     // { field: 'col2', headerName: 'Column 2', width: 150 },
   ];
 
-  const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+  const API_BASE = process.env.NEXT_PUBLIC_API_URL;
   console.log(`${API_BASE}/category`);
   async function fetchCategory() {
-    const data = await fetch(`${APIBASE}/category`);
+    const data = await fetch(`${API_BASE}/category`);
     const c = await data.json();
     const c2 = c.map((category) => {
       category.id = category._id;
@@ -50,7 +50,7 @@ export default function Home() {
   function handleCategoryFormSubmit(data) {
     if (editMode) {
       // data.id = data._id
-      fetch(`${APIBASE}/category`, {
+      fetch(`${API_BASE}/category`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
